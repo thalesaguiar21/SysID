0db79c23442673b560778085a8b6c014a134b249
Thales Aguiar <thalesaguiar21@gmail.com>
2018-10-09 12:42:50 -0300

    Better naming for kalman method

--- sample/estimation.py
+++ sample/estimation.py
@@ -102,34 +102,41 @@ def mat_lse(coef, rs):
     return theta, res, ypred
 
 
-def kalman_filter(A, B, H, u, y, Q=None, R=None):
+def kalman_filter(propag, entry, observation, u,
+                  measures, measurenoise=None, dyn_noise=None):
     """ Compute the Kalman filter
 
     Args:
-        A (ndarray): The state matrix
-        B (ndarray): The exogen matrix
+        propag (ndarray): The propagation matrix
+        entry (ndarray): The entry matrix
         u (ndarray): The system's exogenous inputs
-        y (ndarray): The measured data
-        Q (ndarray): Dynamic noise (defaults to [[0..0],..,[0..0]])
-        R (ndarray): Measure noise (defaults to [[0..0],..,[0..0]])
+        measures (ndarray): The measured data
+        measurenoise (ndarray): Measure noise (defaults to [[0..0],..,[0..0]])
+        dyn_noise (ndarray): Dynamic noise (defaults to [[0..0],..,[0..0]])
 
     Returns:
-        X (ndarray): The system parameters
+        The system parameters through time
 
     """
-    X = zeros(shape=(A.shape[0], 1))
-    P = eye(A.shape[0]) * 1000
-    zeros
-    Q = zeros(A.shape[0]) if Q is None else Q
-    R = zeros(A.shape[0]) if R is None else R
-    Id = eye(len(u))
-    for t in range(len(u)):
+    INITIAL_COVARIANCE = 1000
+    QTD_SAMPLE = len(measures)
+    states = zeros(shape=(propag.shape[0], 1))
+    covariances = eye(propag.shape[0]) * INITIAL_COVARIANCE
+    if measurenoise is None:
+        measurenoise = zeros(propag.shape[0])
+    if dyn_noise is None:
+        dyn_noise = zeros(propag.shape[0])
+
+    identity = eye(QTD_SAMPLE)
+    states_history = []
+    for t in range(QTD_SAMPLE):
         # propagation
-        X = dot(A, X) + dot(B, u[t])
-        P = dot(A, dot(P, A.T)) + Q
+        states = dot(propag, states) + dot(entry, u[t])
+        covariances = dot(propag, dot(covariances, propag.T)) + measurenoise
         # update
-        num = dot(P, H.T)
-        K = num / (dot(H, num) + R)
-        X = X + dot(K, y[t] - dot(H, X))
-        P = dot((Id - dot(K, H)), P)
-    return X
+        numerator = dot(covariances, observation.T)
+        gain = dot(numerator, inv(dot(observation, numerator) + dyn_noise))
+        states = states + dot(gain, measures[t] - dot(observation, states))
+        states_history.append(states)
+        covariances = dot((identity - dot(gain, observation)), covariances)
+    return states_history
