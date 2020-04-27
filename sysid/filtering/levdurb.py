e58f624d033b09a89391ceb0d10afc6fa5199aac
Thales Aguiar <thalesaguiar21@gmail.com>
2020-04-27 17:59:46 -0300

    FIxed some index mistakes for Levinson-Durbin

--- sysid/filtering/levdurb.py
+++ sysid/filtering/levdurb.py
@@ -1,22 +1,25 @@
 import numpy as np 
 
 
-def solve(margs):
-    order = margs.size
-    R = np.append(margs, 0)
-    errors = np.zeros(order + 1) + 1e-15
-    errors[0] = margs[0]
+def solve(corr, order=None):
+    order = corr.size if order is None else order
+    errors = np.zeros(order)
+    errors[0] = corr[0]
+    alpha = np.zeros(order)
+    alpha[0] = 1
 
-    icogs = np.zeros(order + 1)
-    icogs[0] = 1
-    for i in range(1, order+1):
-        alpha_R = 0
-        for j in range(1, i-1):
-            alpha_R += icogs[j] * R[i-j]
-        ki = (R[i]-alpha_R) / errors[i-1]
-        icogs[i] = ki
+    for i in range(1, order):
+        alpha_coef = 0
+        for j in range(1, i):
+            alpha_coef += alpha[j] * corr[i-j]
+
+        ki = (corr[i]-alpha_coef) / errors[i-1]
+        alpha[i] = ki
+
         if i > 1:
-            icogs[1:i] -= ki * np.flip(icogs[1:i])
-        errors[i] = (1 - ki**2)*errors[i]
-    return icogs
+            for j in range(1, i):
+                alpha[j] -= ki*alpha[i-j]
+
+        errors[i] = (1 - ki**2)*errors[i-1]
+    return alpha, errors
 
--- tests/tests_levdurb.py
+++ tests/tests_levdurb.py
@@ -1,6 +1,7 @@
 import unittest
 
 import numpy as np
+from scipy.linalg import solve_toeplitz
 
 from .context import sysid
 from sysid.filtering import levdurb
@@ -11,7 +12,20 @@ class TestsLevdurb(unittest.TestCase):
     def setUp(self):
         pass
 
-    def test_levdurb(self):
+    def test_solution_shape(self):
         margs = np.ones(4)
-        icog = levdurb.solve(margs)
+        coefs, error = levdurb.solve(margs)
+        self.assertEqual(coefs.size, 4)
 
+    def test_result(self):
+        linsys = np.array([1.0, 0.1, -0.8, -0.27])
+        corr = np.array([0.4384, 0.0917, -0.0659, -0.0172])
+        coefs, error = levdurb.solve(corr)
+        toepl = np.array([[1, 0.1, -0.8, -0.27],
+                          [0.1, 1, 0.1, -0.8],
+                          [-0.8, 0.1, 1, 0.1],
+                          [-0.27, -0.8, 0.1, 1]])
+        pred = toepl @ coefs
+        breakpoint()
+        close = all(abs(p-r) < 0.001 for p, r in zip(pred, linsys))
+        self.assertTrue(close, f"\n{pred}\n{linsys} -")
