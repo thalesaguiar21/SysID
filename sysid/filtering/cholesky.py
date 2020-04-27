e667502ff078d7fa529749195cbd999d01256052
Thales Aguiar <thalesaguiar21@gmail.com>
2020-04-27 09:12:22 -0300

    initial cholesky decomposition algorithm

---
+++ sysid/filtering/cholesky.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+
+def decomp(margs, indep):
+    diagonal = diag(margs)
+
+
+def diag(mtx):
+    mdiag = np.zeros(mtx.shape)
+    for i in range(mtx.shape[0]):
+        mdiag[i, i] = mtx[i, i]
+    return mdiag
+
---
+++ tests/tests_cholesky.py
@@ -0,0 +1,30 @@
+import unittest
+
+import numpy as np
+
+from .context import sysid
+from sysid.filtering import cholesky
+
+
+class TestsCholesky(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_cholesky(self):
+        margs = np.ones((4, 4))
+        indep = np.ones((4, 1))
+        icog = cholesky.decomp(margs, indep)
+
+    def test_diagonal(self):
+        margs = np.ones((4, 4))
+        indep = np.ones((4, 1))
+        diag = cholesky.diag(margs)
+        diag_is_one = all(diag[i, i] == 1 for i in range(4))
+        out_is_zero = True
+        for i in range(4):
+            for j in range(4):
+                if i != j:
+                    out_is_zero &= diag[i, j] == 0
+        self.assertTrue(diag_is_one and out_is_zero)
+
